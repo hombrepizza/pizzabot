dc706feae03c66ff84c74791bc98b155f866c202
Cristobal <cristobal.gallegos96@gmail.com>
2020-05-06 23:53:11 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Conectado como ${client.user.tag}`);
+
+});
+
+client.on('message', message => {
+    console.log(message.content);
+    if (message.content === 'funcionas?') {
+        message.reply('si');
+    }
+
+    if (message.content === 'hola') {
+        setTimeout(function () {
+            message.channel.send(`Wena ${message.author}`)
+        }, 5000);
+    }
+});
+
+client.login('NzA3Nzc2NzA4NzgyMDYzNjU2.XrNxDQ.FQH6W7TK3Eb0dvWYZ1_V7TQWpIQ');
