e6f6723ae0efb71d38b3b5566bbba147b3483656
Cristobal <cristobal.gallegos96@gmail.com>
2020-05-07 02:22:01 -0400

    Cuarto commit, ya con musica

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = '!';
+
+const ytdl = require("ytdl-core");
+
+var servers = {};
+
 client.on('ready', () => {
     console.log(`Conectado como ${client.user.tag}`);
 
@@ -19,5 +25,54 @@ client.on('message', message => {
         }, 5000);
     }
 });
+
+client.on('message', message => {
+
+    let args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0]) {
+        case 'play':
+
+            function play(connection, message) {
+                var server = servers[message.guild.id];
+                server.dispatcher = connection.play(ytdl(server.queue[0], { filter: "audioonly" }));
+
+                server.queue.shift();
+
+                server.dispatcher.on("end", function () {
+                    if (server.queue[0]) {
+                        play(connection, message);
+                    } else {
+                        connection.disconnect();
+                    }
+                });
+            }
+
+            if (!args[1]) {
+                message.channel.send("Pon un link .l.");
+                return;
+            }
+            if (!message.member.voice.channel) {
+                message.channel.send("NOASDADA");
+                return;
+            }
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if (!message.guild.voiceConnection) message.member.voice.channel.join().then(function (connection) {
+                play(connection, message);
+            })
+
+            break;
+    }
+
+});
+
+
 
 client.login('NzA3Nzc2NzA4NzgyMDYzNjU2.XrNxDQ.FQH6W7TK3Eb0dvWYZ1_V7TQWpIQ');
